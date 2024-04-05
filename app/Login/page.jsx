ffdfaefd495b3cc9f14751ca612291979406135e
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-04-05 20:46:48 +0100

    commit

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -3,8 +3,8 @@ import Footer from "../components/footer"
 
 function LoginPage() {
   return (
-    <div className='w-full h-full'>
-      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className='w-full'>
+      <div className="flex items-center justify-center bg-gray-50 md:py-12 py-5 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
