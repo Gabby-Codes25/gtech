cc3e1fc7ee3dc3e57db607b8d9163949c1ea9266
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-04-05 20:27:08 +0100

    New commit

--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -3,7 +3,7 @@ import Footer from '../components/footer'
 
 function SignupPage() {
   return (
-    <div>
+    <div className='w-full h-full'>
       <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create an account</h2>
--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -4,20 +4,20 @@ import Link from 'next/link'
 function Footer() {
   return (
     <section>
-        <div className="bg-black text-white flex justify-between py-8 px-8">
+        <div className="w-full h-full bg-black text-white md:flex md:justify-between md:py-8 py-4 px-4 md:px-8">
             <div>
                 <h1 className="pb-3 font-bold">GTech</h1>
                 <p className="w-[300px] text-left">GTech is a shopping center that deals with all kinds of accessories like Phones, TV, Laptops, Headsets, etc. We love our customers and deliver 24/7 anywhere in the country.</p>
             </div>
-            <div className="flex gap-5">
-                <div>
+            <div className="md:flex gap-5">
+                <div className='pb-5'>
                     <p><Link href="/">Home</Link></p>
                     <p><Link href="/">About</Link></p>
                     <p><Link href="/">Contact Us</Link></p>
                     <p><Link href="/">Products</Link></p>
                     <p><Link href="/">Cart</Link></p>
                 </div>
-                <div>
+                <div className='pb-5'>
                     <p><Link href="/">Privacy Policy</Link></p>
                     <p><Link href="/">Careers</Link></p>
                     <p><Link href="/">Terms and Conditions</Link></p>
@@ -25,7 +25,7 @@ function Footer() {
                     <p><Link href="/">Responsibility</Link></p>
                     <p><Link href="/">Delivery</Link></p>
                 </div>
-                <div>
+                <div className='pb-5'>
                     <p><Link href="/">Ps5</Link></p>
                     <p><Link href="/">Ps5 VR</Link></p>
                     <p><Link href="/">Games</Link></p>
