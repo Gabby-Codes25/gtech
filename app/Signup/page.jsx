fecc4d22495610be22daf1bd4586f10fdc81e5f1
Gabriel Obasi <gabrielobasi557@gmail.com>
2024-04-05 20:37:05 +0100

    signup changes

--- app/Signup/page.jsx
+++ app/Signup/page.jsx
@@ -3,8 +3,8 @@ import Footer from '../components/footer'
 
 function SignupPage() {
   return (
-    <div className='w-full h-full'>
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div className='w-full'>
+      <div className="min-h-screen bg-gray-50 flex flex-col justify-center md:py-12 py-5 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create an account</h2>
         </div>
